bece41d3387eaccf6bcf34aa0bff985d02c4b882
adcadams <andrewdcadams@gmail.com>
2020-12-19 10:52:19 -0500

    adds has_many and has_many through to Barber and Customer models

--- app/models/barber.rb
+++ app/models/barber.rb
@@ -1,2 +1,4 @@
 class Barber < ApplicationRecord
+    has_many :appointments
+    has_many :customers, through: :appointments
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,4 @@
 class Customer < ApplicationRecord
+    has_many :appointments
+    has_many :barbers, through: :appointments
 end
