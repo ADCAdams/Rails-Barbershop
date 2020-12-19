97494fc943c22574d1199faa2f84d39e64c74013
adcadams <andrewdcadams@gmail.com>
2020-12-19 12:37:50 -0500

    adds fields_for payment in Customer#new view and updates customers model for payment

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -4,5 +4,24 @@ class CustomersController < ApplicationController
     def new
         @customer = Customer.new
     end
-    
+
+
+    def create
+
+
+    end
+
+    def show
+
+    end
+
+
+    def customer_params
+        params.require(:customer).permit(
+          :username,
+          :password,
+          payment_profiles: [ :cc_name, :cc_number ]
+        )
+      end
+
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,6 @@
 class Customer < ApplicationRecord
     has_many :appointments
+    has_many :payment_profiles
     has_many :barbers, through: :appointments
+    accepts_nested_attributes_for :payment_profiles
 end
