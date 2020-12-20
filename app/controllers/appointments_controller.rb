b64981d36b8b36bf7d00bbbf5247a5acecfa694d
adcadams <andrewdcadams@gmail.com>
2020-12-19 20:04:04 -0500

    appointment accepts nested attbts for barber

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -2,6 +2,7 @@ class AppointmentsController < ApplicationController
 
     def new
         @appointment = Appointment.new
+        @appointment.build_barber
     end
 
 
--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,4 +1,6 @@
 class Appointment < ApplicationRecord
   belongs_to :barber
   belongs_to :customer
+
+  accepts_nested_attributes_for :barber
 end
